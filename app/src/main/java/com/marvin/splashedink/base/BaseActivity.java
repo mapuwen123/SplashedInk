519f7e4c85c81be9abff53070c35cd9c0525bd67
MaPuwen <mapuwen@outlook.com>
2017-07-25 14:28:20 +0800

    Kotlin测试代码

--- app/src/main/java/com/marvin/splashedink/base/BaseActivity.java
+++ app/src/main/java/com/marvin/splashedink/base/BaseActivity.java
@@ -1,13 +1,10 @@
 package com.marvin.splashedink.base;
 
-import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
